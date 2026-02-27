30a6e5af8e983b8eade5a54a1153a072ff5b6a0b
Elias Lopes <ealopes@aggies.ncat.edu>
2026-02-27 10:57:03 -0500

    added vercel.json

---
+++ app/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function  page() {
+  return (
+    <div>
+        <div className="">HOME PAGE</div>
+        <div className="">HOME PAGE</div>
+        <div className="">HOME PAGE</div>
+        <div className="">HOME PAGE</div>
+        <div className="">HOME PAGE</div>
+    </div>
+    
+  )
+}
