d255c4eb13c9aa5795dd0f1edaab9fbef4513a81
Elias Lopes <ealopes@aggies.ncat.edu>
2026-02-27 13:34:09 -0500

    d

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 export default function  page() {
+
+  useEffect(()=>{
+    async function foo(){
+      const url = import.meta.env.VITE_BASE_URL;
+      const d = await fetch(`${url}`, {
+        headers: {"Content-Type" : "application/json"},
+        method: "GET"
+      });
+
+      console.log("DATA:", await d.json());
+    }
+    foo();
+  }, []);
+
   return (
     <div>
         <div className="">HOME PAGE</div>
