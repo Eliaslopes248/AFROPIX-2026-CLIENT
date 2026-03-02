7eb1b79c6411eccfb2cd256be9a6c28244f528aa
Elias Lopes <ealopes@aggies.ncat.edu>
2026-03-02 18:08:13 -0500

    fixed mobile view

--- app/src/pages/LandingPage.jsx
+++ app/src/pages/LandingPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import NavBar from "../components/ui/NavBar";
-import LandingPage_HeroSection from "../components/landing_page_ui/LandingPage_HeroSection";
+import LandingPage_HeroSection from "../components/landing_page_ui/landingpage_herosection";
 import WhySection from "../components/landing_page_ui/why_section";
 import CallingAction from "../components/landing_page_ui/calling_action";
 import Footer from "../components/ui/Footer";
